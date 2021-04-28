e007e09f2d5fe42aec62d58dd1376991b13c1f5c
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2021-04-28 20:58:31 +0200

    responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,20 @@ import Image from 'next/image'
 
 export default function Home() {
     return (
-        <div className="container ml-20">
+        <div className="container ml-5 md:ml-20">
             <div className="mt-20 flex items-end">
                 <Image src="/didrik_white.svg" height={95} width={250}/>
             </div>
             <div className="mt-40">
-                <h1 className="text-7xl text-white font-display">Restful APIs written in Java</h1>
+                <h1 className="text-3xl md:text-7xl text-white font-display">Restful APIs written in Java</h1>
             </div>
             <div className="mt-2">
-                <h1 className="text-7xl text-white font-display">Docker, Kubernetes</h1>
+                <h1 className="text-3xl md:text-7xl text-white font-display">Docker, Kubernetes</h1>
             </div>
             <div className="mt-16">
-                <h1 className="text-9xl text-white font-display">Interested?</h1>
+                <h1 className="text-4xl md:text-9xl text-white font-display">Interested?</h1>
                 <p className="text-white">Sling an email to didrik@didrik.consulting</p>
             </div>
         </div>
     )
 }
-
