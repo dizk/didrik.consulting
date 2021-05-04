09f937e3fd74bbb72b239744f4618912cf73b58e
Didrik A. Rognstad <didrik.rognstad@gmail.com>
2021-05-04 19:36:20 +0200

    add portrait

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,19 @@ export default function Home() {
             <div className="mt-20 flex items-end">
                 <Image src="/didrik_white.svg" height={95} width={250}/>
             </div>
-            {/*<div>*/}
-            {/*    <Image src="/portrait.png" className="object-contain" height={700} width={700} />*/}
-            {/*</div>*/}
-            <div>
+            <div className="mt-24 sm:ml-32 flex">
+                <div className="relative w-72 md:w-96">
+                    <Image src="/portrait.jpg" layout="responsive" className="object-top object-cover rounded-full"
+                           height={1} width={1}/>
+                </div>
+            </div>
+            <div className="mt-32">
                 <h1 className="text-3xl md:text-7xl text-white font-display">Restful APIs written in Java</h1>
             </div>
             <div className="mt-2">
                 <h1 className="text-3xl md:text-7xl text-white font-display">Docker, Kubernetes</h1>
             </div>
-            <div className="mt-16">
+            <div className="mt-24">
                 <h1 className="text-4xl md:text-9xl text-white font-display">Interested?</h1>
                 <p className="text-white">Sling an email to didrik@didrik.consulting</p>
             </div>
